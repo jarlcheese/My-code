b759e6e0c0de26e17b05c784efd8b20e3384def8
jarlcheese <you@example.com>
2024-04-24 15:25:32 -0400

    If statement practice

--- project/hello/if.py
+++ project/hello/if.py
@@ -1,12 +1,12 @@
-temperature = 48
+temperature = input("temperature: ")
 
-if temperature > 80:
+if int(temperature) > 80:
     print("It's a warm day!")
     print("Drink plenty of water!")
-elif temperature > 50: # (50, 80])
+elif int(temperature) > 50: # (50, 80])
     print("It's a nice day!")
     print("Enjoy the outside!")
-elif temperature > 30: # (30, 50]
+elif int(temperature) > 30: # (30, 50]
     print("It's quite chilly outside.")
-elif temperature > 0: # (0, 30])
+elif int(temperature) > 0: # (0, 30])
     print("Too cold! Stay inside!")
